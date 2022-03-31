317a306d719cf599334a331832b1cfb2d593dff1
Lisette Stetson <lisette.stetson@gmail.com>
2022-03-31 19:20:58 -0400

    completed assignment up to stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,40 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+//////Add Item function//////
+function addItem( item ) {
+    console.log( 'in addItem', item );
+    basket.push( item );
+    console.log( item, 'added to basket');
+    return true;
+}//end addItem
+
+console.log( 'Item was added to basket:', addItem( 'monitor' ) );
+console.log( 'Item was added to basket:', addItem( 'dogTreats' ) );
+console.log( 'Item was added to basket:', addItem( 'milk' ) );
+console.log( 'Item was added to basket:', addItem( 'coffee' ) );
+console.log( 'Item was added to basket:', addItem( 'truffles' ) );
+
+//////List Items function//////
+function listItems(){
+    console.log( 'in listItems' );
+    for( let i=0; i<basket.length; i++ ){
+        console.log( basket[i] );
+    }
+    return "list of items complete";
+}//end listItems
+
+console.log( 'these items are in your basket:');
+ console.log( listItems()) ;
+
+//////Empty function//////
+function empty(){
+    console.log( 'in empty', basket );
+    basket = [];
+    console.log( 'your basket has been emptied', basket );
+    return basket
+}
+console.log( 'basket:', basket );
+console.log( 'your current basket', empty() );
